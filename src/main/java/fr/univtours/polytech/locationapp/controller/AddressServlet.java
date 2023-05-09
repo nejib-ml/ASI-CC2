14a7bf443a577a650d290b37037c982d6ae289b2
nejib-ml <mlnejib9@gmail.com>
2023-05-09 12:26:42 +0200

    Autocomplete (suggestion while typing)

--- src/main/java/fr/univtours/polytech/locationapp/controller/AddressServlet.java
+++ src/main/java/fr/univtours/polytech/locationapp/controller/AddressServlet.java
@@ -14,33 +14,64 @@ import javax.servlet.http.HttpServletResponse;
 import fr.univtours.polytech.locationapp.business.AddressBusinessLocal;
 import fr.univtours.polytech.locationapp.model.address.Feature;
 
-@WebServlet("/searchAddress")
-public class AddressServlet extends HttpServlet{
+@WebServlet({ "/searchAddress", "/autocomplete" })
+public class AddressServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@EJB
 	private AddressBusinessLocal addressBusiness;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	        throws ServletException, IOException {
-	    // On place une liste vide dans la requête.
-	    request.setAttribute("ADDRESSES", new ArrayList<Feature>());
-	    // On redirige vers la JSP.
-	    request.getRequestDispatcher("addresses.jsp").forward(request, response);
+			throws ServletException, IOException {
+		String path = request.getServletPath();
+		if ("/autocomplete".equals(path)) {
+			handleAutocomplete(request, response);
+		} else {
+			// On place une liste vide dans la requête.
+			request.setAttribute("ADDRESSES", new ArrayList<Feature>());
+			// On redirige vers la JSP.
+			request.getRequestDispatcher("addresses.jsp").forward(request, response);
+		}
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	        throws ServletException, IOException {
-	    // On récupère ce que l'utilisateur à saisi.
-	    String search = request.getParameter("search");
-	    // On exécute la requête correspondante.
-	    List<Feature> addresses = this.addressBusiness.searchAdresses(search);
-	    // On place le résultat de la recherche dans la requête.
-	    request.setAttribute("ADDRESSES", addresses);
-	    // On redirige vers la JSP.
-	    request.getRequestDispatcher("addresses.jsp").forward(request, response);
+			throws ServletException, IOException {
+		// On récupère ce que l'utilisateur à saisi.
+		String search = request.getParameter("search");
+		// On exécute la requête correspondante.
+		List<Feature> addresses = this.addressBusiness.searchAdresses(search);
+		// On place le résultat de la recherche dans la requête.
+		request.setAttribute("ADDRESSES", addresses);
+		// On redirige vers la JSP.
+		request.getRequestDispatcher("addresses.jsp").forward(request, response);
 	}
+
+	private void handleAutocomplete(HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException, IOException {
+	    String term = request.getParameter("term");
+	    List<Feature> suggestions = this.addressBusiness.searchAdresses(term);
+
+	    response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+
+	    StringBuilder json = new StringBuilder();
+	    json.append("[");
+	    for (int i = 0; i < suggestions.size(); i++) {
+	        json.append("\"").append(escapeJson(suggestions.get(i).getProperties().getName())).append("\"");
+	        if (i < suggestions.size() - 1) {
+	            json.append(",");
+	        }
+	    }
+	    json.append("]");
+
+	    response.getWriter().write(json.toString());
+	}
+
+	private String escapeJson(String input) {
+	    return input.replace("\\", "\\\\").replace("\"", "\\\"");
+	}
+
 
 }
--- src/main/webapp/addresses.jsp
+++ src/main/webapp/addresses.jsp
@@ -3,31 +3,73 @@
 <html>
 <head>
 <title>Rechercher une addresse</title>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
 </head>
 <body>
-    <form action="searchAddress" method="post">
-        <input type="text" name="search"/>
-        <input type="submit" value="Rechercher"/>
-    </form>
-    <table>
-        <tr>
-            <th>Adresse</th>
-            <th>Ville</th>
-            <th>Code postal</th>
-        </tr>
-        <c:forEach var="address" items="${requestScope.ADDRESSES}">
-            <tr>
-                <td>
-                    ${address.properties.name}
-                </td>
-                <td>
-                    ${address.properties.city}
-                </td>
-                <td>
-                    ${address.properties.postcode}
-                </td>
-            </tr>
-        </c:forEach>
-    </table>
+
+	<div class="container mt-4">
+		<h2 class="text-center">Rechercher une addresse</h2>
+		<form action="searchAddress" method="post"
+			class="form-inline justify-content-center my-4">
+			<div class="form-group">
+				<input type="text" class="form-control mr-2" name="search"
+					placeholder="Recherche">
+			</div>
+			<input type="submit" class="btn btn-primary" value="Rechercher">
+		</form>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Adresse</th>
+					<th>Ville</th>
+					<th>Code postal</th>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach var="address" items="${requestScope.ADDRESSES}">
+					<tr>
+						<td>${address.properties.name}</td>
+						<td>${address.properties.city}</td>
+						<td>${address.properties.postcode}</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
+
+	<script type="text/javascript">
+		$(function() {
+			$("input[name='search']").autocomplete({
+				source : function(request, response) {
+					$.ajax({
+						url : "autocomplete",
+						type : "GET",
+						dataType : "json",
+						data : {
+							term : request.term
+						},
+						success : function(data) {
+							response(data);
+						}
+					});
+				},
+				minLength : 2
+			});
+		});
+	</script>
+
 </body>
 </html>
